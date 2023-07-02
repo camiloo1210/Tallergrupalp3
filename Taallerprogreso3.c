87ae4fb5cd5892cf0f5bc536072a7730a3d5951b
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2023-07-02 13:28:53 -0500

    Se mejora la funcion Factura1 para que imprima de mejor manera la factura y se añaden comentarios para entender de mejor manera el funcionamiento del codigo, de igual manera se eliminan 2 funciones para usar 1 sola que optimize el proceso

--- Taallerprogreso3.c
+++ Taallerprogreso3.c
@@ -4,7 +4,7 @@ struct Factura// Estructura de la informacion para la factura
 {
     char Marca[20];
     char Modelo[30];
-    char Opcion [25];
+    char Observaciones [25];
     float Precio; 
 
 };
@@ -14,50 +14,50 @@ struct Usuario// Estructura de la informacion del usuario
     char Direccion[35];
     char Cedula [10];
     };
-
-float Mantenimiento(opcion)//Funcion que devuelve el valor del mantenimiento
+float Precio (opcion)//Funcion que devuelve el valor del mantenimiento
 {
-    if (opcion==1)
+    if (opcion == 1)
     {
         return 30.50;
     }
-}
 
-float Recuperacion(opcion){//Funcion que devuelve el valor de la recuperacion de datos
-    if (opcion==2)
+    if (opcion == 2)
     {
         return 23.75;
     }
-}
-float Formateo(opcion)//Funcion que devuelve el valor del Formateo
-{
-    if (opcion==3)
+
+    if (opcion == 3)
     {
         return 15.50;
     }
-    
 }
 
-void Factura1(struct Factura factura1,struct Usuario usuario1)//Se usa la funcion Factura1 para poder imprimir posteriormente la factura con todos la informacion requerida
+void Factura1(struct Factura factura1,struct Usuario usuario1,int opcion)//Se usa la funcion Factura1 para poder imprimir posteriormente la factura con todos la informacion requerida
 {
+printf("\n");
+printf("\n");
 printf("Factura\n");
+printf("Su nombre es:\n");
 puts(usuario1.Nombre);
-printf("\n");
+printf("Su direccion es:\n");
 puts(usuario1.Direccion);
-printf("\n");
+printf("Su cedula es:\n");
 puts(usuario1.Cedula);
-printf("\n");
+printf("La marca del ordenador es :\n");
 puts(factura1.Marca);
-printf("\n");
+printf("El modelo de la marca es:\n");
 puts(factura1.Modelo);
-printf("\n");
+printf("Las observaciones realizadas fueron las siguientes:\n");
+puts(factura1.Observaciones);
+printf("La opcion que escogiste fue la numero %i\n",opcion);
+printf("El precio total del servicio es de : %f",Precio(opcion));
 }
 
 int main() {
     int opcion;
 
     while (1) {
-        struct Factura factura1;
+       
             struct Usuario usuario1;
             printf("Ingrese su nombre.\n");
             gets(usuario1.Nombre);
@@ -65,11 +65,15 @@ int main() {
             gets(usuario1.Direccion);
             printf("Ingrese su cedula.\n");
             gets(usuario1.Cedula);
-        printf("Ingrese la marca de su ordenador.\n");
-        gets(factura1.Marca);
-        printf("Ingrese el modelo de su ordenador.\n");
-        gets(factura1.Modelo);
-        printf("\n");
+            //Division de informacion
+            struct Factura factura1;
+            printf("Ingrese la marca de su ordenador.\n");
+            gets(factura1.Marca);
+            printf("Ingrese el modelo de su ordenador.\n");
+            gets(factura1.Modelo);
+            printf("Ingrese alguna observacion a tener en cuenta para el servicio de su ordenador.\n");
+            gets(factura1.Observaciones);
+            printf("\n");
             if (1)
             {
             printf("Menú de opciones:\n");
@@ -83,15 +87,15 @@ int main() {
                 switch (opcion) {
                     case 1:
                         printf("Ha seleccionado el mantenimiento del ordenador.\n");
-                        printf("El precio por el servicio seleccionado es %.2f\n",Mantenimiento(opcion));
+                        printf("El precio por el servicio seleccionado es %.2f\n",Precio(opcion));
                         break;
                     case 2:
                         printf("Ha seleccionado la recuperacion de datos.\n");
-                        printf("El precio por el servicio seleccionado es %.2f\n",Recuperacion(opcion));
+                        printf("El precio por el servicio seleccionado es %.2f\n",Precio(opcion));
                         break;
                     case 3:
                         printf("Ha seleccionado el formateo del ordenador.\n");
-                        printf("El precio por el servicio seleccionado es %.2f\n",Formateo(opcion));
+                        printf("El precio por el servicio seleccionado es %.2f\n",Precio(opcion));
                         break;
                     case 4:
                         printf("Saliendo del programa...\n");
@@ -101,9 +105,9 @@ int main() {
                         break;
                     }
             }
-            if (opcion==1 || opcion==2 || opcion==3  )
+            if (opcion==1 || opcion==2 || opcion==3  )//Solo entra a imprimir si alguna de las opciones encaja con la condicion 
             {
-              Factura1(factura1,usuario1);
+              Factura1(factura1,usuario1,opcion);
               return 0;
             } 
     }
