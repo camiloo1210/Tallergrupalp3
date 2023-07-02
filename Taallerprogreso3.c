924899b0651782831e587d2ac6b27f8128f1ff71
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2023-07-02 12:09:11 -0500

    Se incluye la funcion struct para manejar la informacion que se usar para la impresion de la factura donde se incluye la segunda structura de usuario donde se maneja la informacion del cliente como su nombre o su direccion

--- Taallerprogreso3.c
+++ Taallerprogreso3.c
@@ -1,38 +1,65 @@
 #include <stdio.h>
 
+struct Factura
+{
+    struct Usuario
+    {
+    char Nombre[30];
+    char Direccion[35];
+    char Cedula [10];
+    };
+
+    char Marca[20];
+    char Modelo[30];
+    char Opcion [25];
+    float Precio; 
+
+};
+
 int main() {
     int opcion;
 
     while (1) {
-        printf("Menú de opciones:\n");
-        printf("1. Opción 1\n");
-        printf("2. Opción 2\n");
-        printf("3. Opción 3\n");
-        printf("4. Salir\n");
-        printf("Seleccione una opción: ");
-        scanf("%d", &opcion);
+        struct Usuario usuario1;
+        printf("Ingrese su nombre.\n");
+        gets(usuario1.Nombre);
+        printf("Ingrese su direccion.\n");
+        gets(usuario1.Direccion);
+        printf("Ingrese su cedula.\n");
+        gets(usuario1.Cedula);
+        printf("\n");
+            if (1)
+            {
+            printf("Menú de opciones:\n");
+            printf("1. Mantenimiento del ordenador\n");
+            printf("2. Recuperacion de datos\n");
+            printf("3. Formateo del ordenador\n");
+            printf("4. Salir\n");
+            printf("Seleccione una opción: ");
+            scanf("%d", &opcion);
 
-        switch (opcion) {
-            case 1:
-                printf("Ha seleccionado la optimización del ordenador.\n");
-                // Código para la opción 1
-                break;
-            case 2:
-                printf("Ha seleccionado el cambio de software.\n");
-                // Código para la opción 2
-                break;
-            case 3:
-                printf("Ha seleccionado el reinicio del ordenador.\n");
-                // Código para la opción 3
-                break;
-            case 4:
-                printf("Saliendo del programa...\n");
-                return 0;
-            default:
-                printf("Opción inválida. Por favor, seleccione una opción válida.\n");
-                break;
-        }
+            switch (opcion) {
+                case 1:
+                    printf("Ha seleccionado el mantenimiento del ordenador.\n");
+                    // Código para la opción 1
+                    break;
+                case 2:
+                    printf("Ha seleccionado la recuperacion de datos.\n");
+                    // Código para la opción 2
+                    break;
+                case 3:
+                    printf("Ha seleccionado el formateo del ordenador.\n");
+                    // Código para la opción 3
+                    break;
+                case 4:
+                    printf("Saliendo del programa...\n");
+                    return 0;
+                default:
+                    printf("Opción inválida. Por favor, seleccione una opción válida.\n");
+                    break;
+            }
+            }
     }
 
-return 0;
+    return 0;
 }
